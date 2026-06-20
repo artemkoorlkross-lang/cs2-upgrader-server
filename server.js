bf8a4770a2af3c2f0fcbf7f9b71ef241c49b0605
artemkoorlkross-lang <artemkoorlkross@gmail.com>
2026-06-20 13:49:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require("express-session");
 const passport = require("passport");
 const SteamStrategy = require("passport-steam").Strategy;
 
-const STEAM_API_KEY = "38267E8D758B48F36B37D51DF9357999";
+const STEAM_API_KEY =  "38267E8D758B48F36B37D51DF9357999"
 const app = express();
 app.use(express.static("public"));
 
@@ -22,7 +22,7 @@ passport.deserializeUser((user, done) => done(null, user));
 passport.use(new SteamStrategy({
  returnURL: "https://cs2-upgrader-server.onrender.com/auth/steam/return",
 realm: "https://cs2-upgrader-server.onrender.com/",,
-    apiKey: cs2-upgrader-server.onrender.com
+apiKey: STEAM_API_KEY
 }, (identifier, profile, done) => {
     return done(null, profile);
 }));
